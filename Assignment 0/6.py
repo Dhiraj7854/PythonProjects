a5be2484ac1f49c1ef73695187e0008d146a7ef6
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-01-09 17:35:33 +0530

    Update 6.py

--- Assignment 0/6.py
+++ Assignment 0/6.py
@@ -1,4 +1,9 @@
+#PROGRAM TO REVERSE A NUMBER
 n = int(input("Enter a number: "))
+
+#CONVERTING TO STRING
 string = str(n)
+
+#USING string[::-1] TO REVERSE AND PRINT CONVERTING TO int AGAIN
 print(f"Reverse number: {int(string[::-1])}")
 
