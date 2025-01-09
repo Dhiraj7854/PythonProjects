9fab6d93aaefb4ffc596b0f4a220f63b390074c5
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-01-09 17:33:11 +0530

    Update 4th.py

--- Assignment 0/4th.py
+++ Assignment 0/4th.py
@@ -1,5 +1,8 @@
 a = int(input("Enter 1st number: "))
 b = int(input("Enter 2nd number: "))
 print(f"Before swapping a: {a} and b: {b}")
+
+"""SWAPPING 2 VARIABLES WITHOUT 3rd VARIABLE"""
+
 a,b = b,a
 print(f"After swapping a: {a} and b: {b}")
