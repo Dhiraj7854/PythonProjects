7f6facac3d0d87e69db2be95d8061fb283eb66f1
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-01-09 17:34:04 +0530

    Update 5.py

--- Assignment 0/5.py
+++ Assignment 0/5.py
@@ -1,4 +1,7 @@
 num = int(input("ENter any number: "))
 n = int(input("Enter the  number of tables: "))
+
+"""PRINTING TABLES USING LOOP"""
+
 for i in range(1,n+1):
     print(f"{num} * {i} = {num*i}")
