788b951d708f84ba77f184a2224e4de68a1c6fe1
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-01-30 14:54:03 +0530

    Update count.py

--- ASSIGNMENT-2/count.py
+++ ASSIGNMENT-2/count.py
@@ -1,7 +1,10 @@
 text = input("Enter your text: ")
 value = input("Enter value to find: ")
 count = 0
+
 for i in range(len(text)):
-    if (text[i:i+len(value)] == value and text[i+len(value)] == ' '):
-        count += 1
-print("Occurrence:",count)
+    if text[i:i+len(value)] == value:
+        if i + len(value) == len(text) or text[i+len(value)] == ' ':
+            count += 1
+
+print("Occurrence:", count)
