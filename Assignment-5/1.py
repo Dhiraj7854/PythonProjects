b8db1eaae803a9058f72b77684ca82b995a95a00
Dhiraj7854 <dhirajdubey2022@outlook.com>
2025-02-20 13:57:50 +0530

    Update 1.py

--- Assignment-5/1.py
+++ Assignment-5/1.py
@@ -1,25 +1,8 @@
-class Password_manager:
-    def __init__(self):
-        self.old_passwords = []
-
-    def get_password(self):
-        if self.old_passwords:
-            return self.old_passwords[-1]
-        return None
-
-    def set_password(self,new_password):
-        if new_password not in self.old_passwords:
-            self.old_passwords.append(new_password)
-            return True
-        return False
-    
-    def is_correct(self,string):
-        if(string == self.old_passwords[-1]):
-            return True
-        else:
-            return False
-
-pm = Password_manager()
-pm.set_password("Hello123")
-print(pm.get_password())
-print(pm.set_password("Hello123"))
+L = int(input())
+R = int(input())
+MAX = 0
+for i in range(L, R+1):
+    for j in range(L,R+1):
+        if i^j > MAX:
+            MAX = i^j
+print(MAX)
